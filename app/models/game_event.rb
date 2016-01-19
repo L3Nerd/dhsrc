b6be33c398d881f535f81faa976fb052ded7c195
Lennart Kindermann <lennart.kindermann@cloudseeds.de>
2016-01-19 18:57:34 +0100

    added Comment Model and added associate attributes

--- app/models/game_event.rb
+++ app/models/game_event.rb
@@ -1,5 +1,6 @@
 class GameEvent < ActiveRecord::Base
   belongs_to :team
   belongs_to :venue
+  has_many :comments, as: :commentable, :dependent => :destroy
   has_many :players, through: :player_in_games
 end
--- app/models/training_event.rb
+++ app/models/training_event.rb
@@ -1,5 +1,5 @@
 class TrainingEvent < ActiveRecord::Base
   has_one :training
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, :dependent => :destroy
   has_and_belongs_to_many :player, counter_cache: true
 end
