cba336c8c57b7c73d07f370597866bcb67ba1217
Lennart Kindermann <lennart.kindermann@cloudseeds.de>
2016-01-19 18:24:11 +0100

    added additional columns

--- db/migrate/20160119132244_create_player_in_teams.rb
+++ db/migrate/20160119132244_create_player_in_teams.rb
@@ -3,6 +3,7 @@ class CreatePlayerInTeams < ActiveRecord::Migration
     create_table :player_in_teams do |t|
       t.references :player, index: true, foreign_key: true
       t.references :team, index: true, foreign_key: true
+      t.integer :position, null: false, default: 1
       t.boolean :is_captain
 
       t.timestamps null: false
--- db/migrate/20160119144107_create_training_events.rb
+++ db/migrate/20160119144107_create_training_events.rb
@@ -2,6 +2,7 @@ class CreateTrainingEvents < ActiveRecord::Migration
   def change
     create_table :training_events do |t|
       t.date :day, null: false
+      t.date :lock_date, null: false
       t.integer :players_count, default: 0, null: false
 
       t.timestamps null: false
