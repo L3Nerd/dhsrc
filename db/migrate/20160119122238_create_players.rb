f8e96cc2caee5b7ef003f5a2dfa9d0f711c743aa
Lennart Kindermann <lennart.kindermann@cloudseeds.de>
2016-01-19 13:33:20 +0100

    added license_expires to Player Model

--- db/migrate/20160119122238_create_players.rb
+++ db/migrate/20160119122238_create_players.rb
@@ -3,6 +3,7 @@ class CreatePlayers < ActiveRecord::Migration
     create_table :players do |t|
       t.date :joined_at
       t.string :dsqv_nr
+      t.date :license_expires
       t.references :user, index: true, foreign_key: true
       t.references :license, index: true, foreign_key: true
       t.references :age_class, index: true, foreign_key: true
